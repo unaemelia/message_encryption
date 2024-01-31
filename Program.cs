516e09b0be66afb6962fa83213688dbf18eaf721
Una Árnadóttir <unaemelia@gmail.com>
2024-01-31 15:54:58 +0100

    initialize feature/encrypt-function branch

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/encrypt", () => "Encrypt function!");
 
 app.Run();
